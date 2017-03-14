a569c950fb588ca9ed8833511ee7afea4190efb8
Son <dethichthien@me.com>
2017-03-14 08:44:40 +0700

    cho len thoi

--- assets/Script/GameScript/GameTouchScript.js
+++ assets/Script/GameScript/GameTouchScript.js
@@ -11,11 +11,18 @@ cc.Class({
     },
 
     playSoundTouch:function(){
-        cc.audioEngine.stopAll();
-        cc.audioEngine.playEffect(this.soundTouch);
+        cc.loader.loadRes("Sound/gametouch/ta", function (err, audiofile) {
+            //cc.log("----  %s ",err);
+            cc.audioEngine.stopAll();
+            cc.audioEngine.playEffect(audiofile);
+        });
+
+
     },
 
-
+    onDisable: function onDisable() {// bat buoc phai co de giai phong bo nho
+        cc.loader.releaseRes("Sound/gametouch/ta");
+    },
 
     // called every frame, uncomment this function to activate update callback
     // update: function (dt) {
