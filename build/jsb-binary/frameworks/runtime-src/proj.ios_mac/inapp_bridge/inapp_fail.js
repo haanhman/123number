f44a42bf9824179d4d312c3c9eda735e02b73a4c
Ha Anh Man <anhmantk@gmail.com>
2017-03-24 05:13:54 +0700

    xoa bo code native ios

--- build/jsb-binary/frameworks/runtime-src/proj.ios_mac/inapp_bridge/inapp_fail.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by anhmantk on 3/3/17.
- */
-var currentsc = cc.director.getScene();
-var runningScene = currentsc.children[0].getComponent("HomeUIScript");
-runningScene.unlockDataError();
