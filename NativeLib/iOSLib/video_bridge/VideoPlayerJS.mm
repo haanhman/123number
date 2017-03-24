95976b959bf1b4aed14d1d0aaaacbd09f94dc1ff
Son <dethichthien@me.com>
2017-03-24 10:17:09 +0700

    thêm nền trắng và sửa size cho button

--- NativeLib/iOSLib/video_bridge/VideoPlayerJS.mm
+++ NativeLib/iOSLib/video_bridge/VideoPlayerJS.mm
@@ -7,6 +7,10 @@
 -(void)openVideoPlayer:(NSString*)stringVideoName {
     UIViewController *rootView = [UIApplication sharedApplication].keyWindow.rootViewController;
     
+    NSLog(@"stringVideoName: %@",stringVideoName);
+    if ([[NSFileManager defaultManager] fileExistsAtPath:stringVideoName]) {
+        NSLog(@"video not found");
+    }
     NSURL *urlVideoFile = [NSURL fileURLWithPath:stringVideoName];
     AVPlayerItem *item = [[AVPlayerItem alloc] initWithURL:urlVideoFile];
     _playerViewController = [[AVPlayerViewController alloc] init];
@@ -22,6 +26,7 @@
     }];
     
     [self createHomeButton];
+    _playerViewController.view.backgroundColor=[UIColor whiteColor];
     [rootView.view addSubview:_playerViewController.view];
     rootView.view.autoresizesSubviews = YES;
     
@@ -30,10 +35,19 @@
 
 -(void)createHomeButton {
     CGSize sceneSize = [UIScreen mainScreen].bounds.size;
-    
-    UIImage *homeImg = [UIImage imageNamed:@"close.png"];
+    UIImage *homeImg = [UIImage imageNamed:@"closebutton.png"];
     UIImageView *homeButton = [[UIImageView alloc] initWithImage:homeImg];
-    [homeButton setFrame:CGRectMake(sceneSize.width - 45, 5, 40, 40)];
+    
+    // app cua minh bat ke man hinh nao thi deu co do cao la 640
+    // button size 40x40 _>retina =80*80 tile la 40/640;
+    float sc_scale=sceneSize.height/320.0f;
+    NSLog(@"sc_scale: %f",sc_scale);
+    float size_h=sceneSize.height*80/640.0f;
+    float size_w=homeImg.size.width*size_h/homeImg.size.height;
+    
+    
+    
+    [homeButton setFrame:CGRectMake(sceneSize.width - 45*sc_scale, 5*sc_scale,size_w, size_h)];
     
     UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(backToHomeScene)];
     [homeButton setUserInteractionEnabled:YES];
