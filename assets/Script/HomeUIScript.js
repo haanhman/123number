2fcf3ce130accbd03dd47cbfdf9bbb941d5c4ea5
Ha Anh Man <anhmantk@gmail.com>
2017-04-25 05:19:48 +0700

    gheo giao dien trang chu, popop setting, parental

--- assets/Script/HomeUIScript.js
+++ assets/Script/HomeUIScript.js
@@ -5,122 +5,17 @@ cc.Class({
     extends: vkidsScene,
 
     properties: {
-        popOptionPrefab: {
-            type: cc.Prefab,
-            default: null
-        },
-
-        cardAtlas: {
-            default: null,
-            type: cc.SpriteAtlas
-        },
-        bgDisplay: {
-            type: cc.Node,
-            default: null
-        },
-        btnBuy: cc.Sprite,
-        bgDisableTouch: cc.Sprite,
-        installData: cc.Prefab,
-        RatePopup: cc.Prefab,
-        SalePopup: cc.Prefab,
         parental: cc.Prefab,
-        markLock: cc.Prefab,
-        rqdownload: cc.Prefab,
-        contentNode: cc.Node,
-        btnMoreApp: cc.Sprite
+        setting: cc.Prefab,
+        btnBuy: cc.Sprite
     },
 
-    configDisplay: function () {
-        //var sizesc=cc.director.getVisibleSize();
-        //if(sizesc.height>800){
-        //    this.bgDisplay.scaleY=sizesc.height/800;
-        //}
-    },
-    // use this for initialization
-
-    reloadAllCard: function () {
-        var limitFree = null;
-        var limitRateToUnlock = null;
-        limitFree = Utils.limitFree();
-        limitRateToUnlock = Utils.limitRateToUnlock();
-        cc.log('limitFree: ' + limitFree.join(' - '));
-        cc.log('limitRateToUnlock: ' + limitRateToUnlock.join(' - '));
-
-        var allPage = this.contentNode.children;
-        for (var pid in allPage) {
-            var page_node = allPage[pid];
-            var cards = page_node.children;
-            for (var ic in cards) {
-                var tmp_card = cards[ic];
-                tmp_card.removeAllChildren(true);
-                tmp_card.cardType = "free";
-                //xoa tat ca nhung phan tu con truoc khi kiem tra luat mua ban
-                tmp_card.removeAllChildren(true);
-                var letter = tmp_card.name.toLowerCase();
-
-                if (limitFree.indexOf(letter) >= 0) {
-                    tmp_card.removeAllChildren(true);
-                } else if (limitRateToUnlock.indexOf(letter) >= 0) {
-                    /*
-                     * Kiểm tra đã rate app hay chua
-                     * - nếu rate rồi
-                     *   + đã download hay chua
-                     * - nếu chưa rate, khi ấn vào card popup sẽ phải hiển thị nut rate app
-                     * */
-
-                    if (!Utils.checkRateApp()) {
-                        var rdl = cc.instantiate(this.rqdownload);
-                        tmp_card.addChild(rdl);
-                        tmp_card.cardType = "rate";
-                    }
-                } else {
-                    // neu chua mua thi hien thi mark lock
-                    // mua roi ma chua down thi hien thi icon download
-                    // download roi thi cho choi luon
-                    if (!Utils.isUnlockContent()) {
-                        var rdl = cc.instantiate(this.markLock);
-                        tmp_card.addChild(rdl);
-                        tmp_card.cardType = "buy";
-                    } else {
-                        if (Utils.checkDownload(letter)) {
-                            tmp_card.removeAllChildren(true);
-                        } else {
-                            var rdl = cc.instantiate(this.rqdownload);
-                            tmp_card.addChild(rdl);
-                        }
-                    }
-
-                }
-            }
-        }
-    },
 
     onLoad: function () {
-        if (cc.sys.os == cc.sys.OS_OSX) {
-            cc.sys.localStorage.removeItem('vkids_buy_content');
-            cc.sys.localStorage.removeItem('vkids_rated');
-            cc.sys.localStorage.removeItem('vkids_need_rate_app');
-        }
 
-        this.checkRateConfig();
-
-        this.configDisplay();
-        if (Utils.isUnlockContent()) {
-            this.btnMoreApp.node.active = true;
-            this.btnMoreApp.node.opacity = 255;
-            this.removeBuyBtn();
-        }
-        this.reloadAllCard();
     },
     removeBuyBtn: function () {
         this.btnBuy.node.removeFromParent(true);
-
-        var sequence = [];
-        sequence.push(cc.delayTime(1.0));
-        sequence.push(cc.fadeIn(0.3));
-
-        this.btnMoreApp.node.active = true;
-        this.btnMoreApp.node.runAction(cc.sequence(sequence));
     },
 
     actionShare: function () {
@@ -136,6 +31,7 @@ cc.Class({
         this.addParentalPopup('feedback', this);
     },
     actionBuyNow: function() {
+        Utils.soundButton();
         this.addParentalPopup('buy', this);
     },
     actionRestore: function() {
@@ -144,32 +40,6 @@ cc.Class({
     actionBuyAll: function () {
         this.addPrefabs(this.SalePopup, "sale_popup", undefined, this);
         return;
-    },
-    actionClickCard: function (nodebutton) {
-        if(nodebutton.target.cardType == 'rate') {
-            this.addPrefabs(this.RatePopup, "rate_popup", undefined, this);
-            return;
-        }
-        if(nodebutton.target.cardType == 'buy') {
-            this.addPrefabs(this.SalePopup, "sale_popup", undefined, this);
-            return;
-        }
-        var namebutton = nodebutton.target.name;
-        namebutton = namebutton.toLocaleLowerCase();
-
-        var checkOldNode = this.node.getChildByName("PopOptions");
-        if (checkOldNode == null) {
-            var popnode = this.addPrefabs(this.popOptionPrefab, "PopOptions");
-
-            var scriptCard = popnode.getComponent("PopScript");
-            scriptCard.cardSprite.spriteFrame = this.cardAtlas.getSpriteFrame("characters-" + namebutton);
-            scriptCard.strCardName = namebutton;
-            scriptCard.lbtitle.string = "Letter " + namebutton.toUpperCase();
-            scriptCard.selectedLetter = namebutton.toLowerCase();
-            scriptCard.validateCardInfo();
-        } else {
-            cc.log("lock add new popup");
-        }
     },
     unlockDataSuccess: function () {
         cc.log("Nguoi dung da unlock data thanh cong");
@@ -182,15 +52,9 @@ cc.Class({
         cc.log("Unlock data that bai");
     },
 
-    activeBgNoTouch: function (active) {
-        this.bgDisableTouch.node.active = active;
-        this.bgDisableTouch.node.setLocalZOrder(11);
-    },
 
-    bgClickAction: function () {
-        cc.log("====> Bg click");
-    },
 
+    /*
     start: function () {
         this.checkInstallData();
         if(Utils.isUnlockContent() == null && Utils.loadProduct == false) {
@@ -230,11 +94,10 @@ cc.Class({
         cc.log("removePopupInstallData");
         this.node.getChildByName("InstallDataPopup").removeFromParent(true);
     },
-
+    */
     addParentalPopup: function (action, parent) {
         this.addPrefabs(this.parental, "parental", action, parent);
     },
-
     addPrefabs: function (prefabs, name, action, parent) {
         var popup = cc.instantiate(prefabs);
         if(parent != undefined) {
@@ -272,6 +135,15 @@ cc.Class({
             }
 
         }.bind(this));
-    }
+    },
+
+    actionSetting: function () {
+        Utils.soundButton();
+        this.addPrefabs(this.setting, "setting", undefined, this);
+    },
+    switchLearn: function () {
+        Utils.soundButton();
+    },
+
 
 });
