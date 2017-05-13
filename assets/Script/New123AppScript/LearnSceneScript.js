07a879d67376110f480f032e65a0d97b4ff336af
Son <dethichthien@me.com>
2017-05-13 21:31:15 +0700

    cap nhat ui learn

---
+++ assets/Script/New123AppScript/LearnSceneScript.js
@@ -0,0 +1,52 @@
+var Utils = require('Utils');
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        nodeSLNumber:cc.Node,
+        lbCount:cc.Label,
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        this.nodeSLNumber.setLocalZOrder(-10);
+        this.nodeSLNumber.active=false;
+        if(Utils.rangeNumber=="undefined"){
+            Utils.rangeNumber=5;
+        }else if(Utils.rangeNumber==null){
+            Utils.rangeNumber=5;
+        }
+
+    },
+
+    actionClose:function(){
+
+    },
+    actionSlectNumber:function(){
+        this.nodeSLNumber.active=!this.nodeSLNumber.active;
+    },
+    actionTracing:function(){
+
+    },
+    actionLearning:function(){
+
+    },
+    actionCounting:function(){
+        cc.director.loadScene("Learn123.fire");
+    },
+
+
+    actionSelect15:function(){
+        this.nodeSLNumber.active=false;
+        this.lbCount.string="5";
+        Utils.rangeNumber=5;
+    },
+    actionSelect110:function(){
+        this.nodeSLNumber.active=false;
+        this.lbCount.string="10";
+        Utils.rangeNumber=10;
+    },
+
+
+
+});
---
+++ assets/Script/New123AppScript/Learning123Script.js
@@ -0,0 +1,27 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        lbNumber:cc.Label,
+
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+
+    actionClose:function(){
+        cc.director.loadScene("LearnSC.fire");
+    },
+    actionNext:function(){
+
+    },
+    actionBack:function(){
+
+    },
+    actionSound:function(){
+
+    },
+});
